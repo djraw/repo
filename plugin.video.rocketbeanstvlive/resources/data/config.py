6d2720a6f0e841fe6fb96cbfd39018dfdabf53a7
R.A.W <raw1@gmx.de>
2019-09-28 17:13:02 +0200

    Update config.py

--- plugin.video.rocketbeanstvlive/resources/data/config.py
+++ plugin.video.rocketbeanstvlive/resources/data/config.py
@@ -1,3 +1,3 @@
-CHANNEL_ID = 'ROCKETBEANSTV'
+CHANNEL_ID = 'rocketbeanstv'
 LETS_PLAY_CHANNEL_ID = 'UCtSP1OA6jO4quIGLae7Fb4g'
 GUIDE_URL = 'http://api.rbtv.rodney.io/api/1.0/schedule/schedule_linear.json'
